0c40018233ab907c319fb3a999239ad35434aa4d
Christopher Topalian <58909034+ChristopherTopalian@users.noreply.github.com>
2024-11-17 00:21:24 -0500

    Add files via upload

---
+++ project001/cos/src/math/divide.js
@@ -0,0 +1,17 @@
+// divide.js
+
+function divide(a, b)
+{
+    return a / b;
+}
+
+module.exports = divide;
+
+//----//
+
+// Dedicated to God the Father
+// All Rights Reserved Christopher Andrew Topalian Copyright 2000-2024
+// https://github.com/ChristopherTopalian
+// https://github.com/ChristopherAndrewTopalian
+// https://sites.google.com/view/CollegeOfScripting
+
