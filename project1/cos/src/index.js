0c2cb13e382938d6643508207906fbdadea07bd2
Christopher Topalian <58909034+ChristopherTopalian@users.noreply.github.com>
2024-11-17 00:27:40 -0500

    Add files via upload

---
+++ project1/cos/src/index.js
@@ -0,0 +1,18 @@
+// index.js
+
+const divide = require('./math/divide');
+const multiply = require('./math/multiply');
+
+module.exports = {
+    divide,
+    multiply,
+};
+
+//----//
+
+// Dedicated to God the Father
+// All Rights Reserved Christopher Andrew Topalian Copyright 2000-2024
+// https://github.com/ChristopherTopalian
+// https://github.com/ChristopherAndrewTopalian
+// https://sites.google.com/view/CollegeOfScripting
+
